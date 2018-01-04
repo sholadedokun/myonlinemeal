891477fb2406ed4e2e75576a76a613d6eeba943e
Olushola Adedokun <sholadedokun@yahoo.com>
2018-01-04 15:45:00 +0100

    added subscription history

--- src/js/components/dashboard.js
+++ src/js/components/dashboard.js
@@ -1,35 +1,58 @@
 import React, {Component} from 'react';
-import {Col} from 'react-bootstrap';
+import {Col, Row} from 'react-bootstrap';
 import Heading from './heading'
 import {getSubScription} from '../actions/subScriptionActions'
 import {connect} from 'react-redux'
 import {modalStatus} from '../actions/userActions'
+import _ from 'lodash'
+import moment from 'moment';
 class Dashboard extends Component{
     constructor(props){
         super();
     }
     componentWillMount(){
         this.props.getSubScription().then(data =>{
-
             if(data==null){
                 this.props.modalStatus(true, 'plans')
             }
 
+
         })
     }
     render(){
-        const {allPlans}=this.props
+        const {Subscription}=this.props
         return(
-            <Col xs="12" className="section dashboard">
-                <Heading size="md" title="USERS Dashboards" marginBottom="2em"/>
-
-            </Col>
+            <Row className="dashboard">
+                <Col xs="12">
+                    <Heading size="md" title="USERS Dashboards" marginBottom="2em"/>
+                        <Heading size="sm" title="Subscription History" marginBottom="1em" />
+                        <Col xs="12" className="historyHeading">
+                            <Col xs="2">Status</Col>
+                            <Col xs="2">Type</Col>
+                            <Col xs="2">Starts</Col>
+                            <Col xs="2">Ends</Col>
+                            <Col xs="2">Meals Delivered</Col>
+                            <Col xs="2">Meals Remaining</Col>
+                        </Col>
+                        <Col xs="12" componentClass="ul" className="subscriptionList">
+                            {
+                                _.map(Subscription, (item, index)=>
+                                    <Col xs="12" componentClass="li" key={index}>
+                                        <Col xs="2">{item.status}</Col>
+                                        <Col xs="2">{item.planId.name}</Col>
+                                        <Col xs="2">{moment(item.subscribedDate).fromNow()}</Col>
+                                    </Col>
+                                )
+                            }
+                        </Col>
+                </Col>
+            </Row>
         )
     }
 }
 function mapStateToProps(state){
     return{
-        currentSubscription:state.subScription.subScriptions
+        Subscription:state.subScription.subScriptions
     }
 }
 export default connect(mapStateToProps, {getSubScription, modalStatus})(Dashboard)
